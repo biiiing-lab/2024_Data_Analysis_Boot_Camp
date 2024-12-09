c8298e3a2a33caec00e4e96299fa7e19a3226a83
jangy <jangyb0901@gmail.com>
2024-12-09 16:59:35 +0900

    연습 문제 풀이

---
+++ CH01_Grammer/Q01.py
@@ -0,0 +1,11 @@
+univ = "한국대학교"
+dept = "경제학과"
+name = "홍길동"
+phone = "010-1234-5678"
+
+print(f'학교 : {univ}')
+print(f'학과 : {dept}')
+print(f'이름 : {name}')
+print(f'전화번호 : {phone}')
+
+print(f'{name} 학생은 {univ} {dept}에 재학 중이며, 연락처를 {phone}입니다.')
